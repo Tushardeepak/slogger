6667b240ce1599111a6bc7857025c021f7e06d11
Tushar Deepak <39951979+Tushardeepak@users.noreply.github.com>
2021-02-14 15:34:47 +0530

    notPwa

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,42 +1,42 @@
-const CACHE_NAME = "version-1";
-const urlsToCache = ["index.html", "offline.html"];
+// const CACHE_NAME = "version-1";
+// const urlsToCache = ["index.html", "offline.html"];
 
-const self = this;
+// const self = this;
 
-//Install
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("opened cache");
+// //Install
+// self.addEventListener("install", (event) => {
+//   event.waitUntil(
+//     caches.open(CACHE_NAME).then((cache) => {
+//       console.log("opened cache");
 
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
+//       return cache.addAll(urlsToCache);
+//     })
+//   );
+// });
 
-//Listen
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(() => {
-      return fetch(event.request).catch(() => caches.match("offline.html"));
-    })
-  );
-});
+// //Listen
+// self.addEventListener("fetch", (event) => {
+//   event.respondWith(
+//     caches.match(event.request).then(() => {
+//       return fetch(event.request).catch(() => caches.match("offline.html"));
+//     })
+//   );
+// });
 
-//Activate
-self.addEventListener("activate", (event) => {
-  const cacheWhitelist = [];
-  cacheWhitelist.push(CACHE_NAME);
+// //Activate
+// self.addEventListener("activate", (event) => {
+//   const cacheWhitelist = [];
+//   cacheWhitelist.push(CACHE_NAME);
 
-  event.waitUntil(
-    caches.keys().then((cacheNames) =>
-      Promise.all(
-        cacheNames.map((cacheName) => {
-          if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
-          }
-        })
-      )
-    )
-  );
-});
+//   event.waitUntil(
+//     caches.keys().then((cacheNames) =>
+//       Promise.all(
+//         cacheNames.map((cacheName) => {
+//           if (!cacheWhitelist.includes(cacheName)) {
+//             return caches.delete(cacheName);
+//           }
+//         })
+//       )
+//     )
+//   );
+// });
